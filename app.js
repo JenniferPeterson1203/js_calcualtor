d2e90ae4b6964f87b4231a576581c5cfa419e813
Jennifer <jenniferpeterson@pursuit.org>
2024-11-01 04:57:37 -0400

    select the buttons

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to My App</h1>
-    </div>
-  );
-}
-
-export default App;
+const screenDisplay = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+// console.log(screenDisplay);
+console.log(buttons);
