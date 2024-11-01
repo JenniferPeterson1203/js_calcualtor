cdc38f44a23f5132a4966cea8acfe0295f0f9171
Jennifer <jenniferpeterson@pursuit.org>
2024-11-01 05:01:41 -0400

    add an event listener to each button

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const screenDisplay = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button");
 // console.log(screenDisplay);
-console.log(buttons);
+// console.log(buttons);
+
+function calculate(button) {
+  console.log("clicked", button);
+}
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
